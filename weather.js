1a55ee61c4110214bd32db93edaf7be34bfcef21
Ayoub <ayoub9901az@gmail.com>
2023-11-10 20:24:39 +0200

    Upload file

---
+++ weather.js
@@ -0,0 +1,53 @@
+const apikey = "57eebf19fc7a4f006c20c22bea8bd3a5";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const WeatherIcon = document.querySelector(".weather-icon");
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function CheckWeather(city){
+    const response = await fetch(apiUrl + city +`&appid=${apikey}`);
+    var data = await response.json();
+
+    
+    if(response.status == 404){
+        document.querySelector(".weather").style.display = "none";
+        document.querySelector(".error").style.display = "block";
+
+
+    }else{
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    
+    if(data.weather[0].main == "Clouds"){
+        WeatherIcon.src = "images/clouds.png";
+    }else
+    if(data.weather[0].main == "Clear"){
+        WeatherIcon.src = "images/clear.png";
+    }else
+    if(data.weather[0].main == "Drizzle"){
+        WeatherIcon.src = "images/drizzle.png";
+    }else
+    if(data.weather[0].main == "Mist"){
+        WeatherIcon.src = "images/mist.png";
+    }else
+    if(data.weather[0].main == "Rain"){
+        WeatherIcon.src = "images/rain.png";
+    }else
+    if(data.weather[0].main == "Snow"){
+        WeatherIcon.src = "images/snow.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+
+    }
+
+}
+
+searchBtn.addEventListener("click",()=>{
+
+    CheckWeather(searchBox.value);
+})
